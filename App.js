7c275170c4483593bc93de2ba4dc4f721ef50123
GhulamMustafaBloch <ghulammustafabloch0302666@gmail.com>
2023-07-20 22:42:38 +0500

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet} from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import Home from './Components/Home';
+import About from './Components/About';
 
 export default function App() {
   const stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
       <stack.Navigator>
-        <stack.Screen name="Home" component={Home} />
+        <stack.Screen name="Al Quran" component={Home} />
+        <stack.Screen name="About" component={About} />
       </stack.Navigator>
     </NavigationContainer>
   );
--- Components/Home.js
+++ Components/Home.js
@@ -1,56 +1,48 @@
-import {ScrollView, StyleSheet, Text, View} from 'react-native'
-import React, {useEffect, useState} from 'react'
-import axios from 'axios';
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styles from "../styles/homeStyles";
 
-export default function Home() {
-    const [data, setData] = useState([])
-    useEffect(() => {
-        axios.get(`https://api.alquran.cloud/v1/surah`).then((response) => {
-            setData(response.data.data);
-            console.log(response.data.data)
-        })
-    }, []);
-    return(
-        <View>
-            <ScrollView>
-            {
-                data.length ? <View>
-                    {
-                        data.map((surahs)=>{
-                            const {name,number} = surahs
-                            return(
-                                <View style={styles.surahContainer}>
-                                    <Text style={styles.surahsText}>{name}</Text>
-                                    <Text style={styles.surahsText}>{number}</Text>
-                                </View>
-                            )
-                        }
-                        )
-                    }
-                </View>:
-                <Text>data is lodding...</Text>
-            }
-            </ScrollView>
-        </View>
-    )
+export default function Home(props) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.alquran.cloud/v1/quran/ar.alafasy`)
+      .then((response) => {
+        setData(response.data.data.surahs);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <View>
+      <ScrollView>
+        {data.length ? (
+          <View>
+            {data.map((surah, index) => {
+              const { name, number, ayahs } = surah;
+              return (
+                <TouchableOpacity
+                  key={index}
+                  onPress={() => {
+                    props.navigation.navigate("About", { SurahText: ayahs });
+                  }}
+                >
+                  <View style={styles.surahContainer}>
+                    <Text style={styles.surahsText}>{name}</Text>
+                    <Text style={styles.surahsText}>{number}</Text>
+                  </View>
+                </TouchableOpacity>
+              );
+            })}
+          </View>
+        ) : (
+          <Text>data is loading...</Text>
+        )}
+      </ScrollView>
+    </View>
+  );
 }
-
-const styles = StyleSheet.create({
-    surahContainer:{
-        paddingHorizontal: 20,
-        marginVertical:5,
-        marginHorizontal:40,
-        backgroundColor: "green",
-        flex: 1,
-        flexDirection: "row-reverse",
-        alignItems: "center",
-        justifyContent: "space-between",
-        height: 60,
-        borderRadius:5,
-        direction: "rtl",
-    },
-    surahsText:{
-        color: "white",
-        fontSize: 20,
-    }
-})
