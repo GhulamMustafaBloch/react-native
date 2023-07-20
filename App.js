6d2016d681eb04130241585595d5b7baa4246970
GhulamMustafaBloch <ghulammustafabloch0302666@gmail.com>
2023-07-20 22:49:50 +0500

    first commit

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import Home from './Components/Home';
 import About from './Components/About';
-
 export default function App() {
   const stack = createNativeStackNavigator();
   return (
@@ -15,6 +14,5 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
 const styles = StyleSheet.create({
 });
